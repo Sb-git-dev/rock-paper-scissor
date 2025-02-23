0aa2ae28e4e21f773265d6b8781fc63e584bc8f5
SB-git-dev <madhva.2114@gmail.com>
2025-02-23 23:54:56 +0530

    Basic implementation of a Rock Paper Scissor game with HTML and Javascript. -Learned and implemented DOM manipulation -Understanded use of basic event handlers for learning event object and their manipulation -Add CSS

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let humanScore = 0;
 let computerScore = 0;
+let gameOver = false;
 function getComputerChoice()
 {
 
@@ -18,84 +19,84 @@ function getComputerChoice()
     console.log(choice);
     return choice;
 }
-function getHumanChoice()
-{
-    let choice = prompt("Choose among Rock, Paper and Scissor").toLowerCase().trim();
-    console.log(choice);
-    return choice;
-}
+
 function playRound(humanChoice, computerChoice)
 {
-    switch(humanChoice)
-    {
-        case "rock":if(computerChoice == "scissor"){
-                        console.log("You won! Rock beats Scissor");
-                        humanScore++;
-                    }
-                    else if(computerChoice == "paper"){
-                        console.log("You lose! Paper beats Rock");
-                        computerScore++;
-                    }
-                    else{
-                        console.log("Tie");
-                    }
+    const div = document.querySelector("div");
+    if(gameOver == false){
+        switch(humanChoice)
+        {
+            case "rock":if(computerChoice == "scissor"){
+                            humanScore++;
+                            div.textContent="You won! Rock beats Scissor "+" Human Score "+humanScore;
+                            
+                        }
+                        else if(computerChoice == "paper"){
+                            computerScore++;
+                            div.textContent="You lose! Paper beats Rock"+" Computer Score "+computerScore;
+                        }
+                        else{
+                            div.textContent = "Tie";
+                        }
+                        break;
+            case "paper":if(computerChoice == "scissor"){
+                            computerScore++
+                            div.textContent = "You lose! Scissor beat paper"+" Computer Score "+computerScore;
+                        }
+                        else if(computerChoice == "rock"){
+                            humanScore++;
+                            div.textContent = "You won! Paper beats rock"+" Human Score "+humanScore;
+                        }
+                        else{
+                            div.textContent = "Tie";
+                        }
+                        break;
+            case "scissor":if(computerChoice == "paper"){
+                            humanScore++;
+                            div.textContent = "You won! Scissor beats Paper"+" Human Score "+humanScore;
+                        }
+                        else if(computerChoice == "rock"){
+                            computerScore++;
+                            div.textContent = "You lose! Rock beats paper"+" Computer Score "+computerScore;
+                        }
+                        else{
+                            div.textContent = "Tie";
+                        }
+                        break;
+            default:div.textContent = "Invalid input";
                     break;
-        case "paper":if(computerChoice == "scissor"){
-                        console.log("You lose! Scissor beats paper");
-                        computerScore++;
-                    }
-                    else if(computerChoice == "rock"){
-                        console.log("You won! Paper beats rock");
-                        humanScore++;
-                    }
-                    else{
-                        console.log("Tie");
-                    }
-                    break;
-        case "scissor":if(computerChoice == "paper"){
-                        console.log("You won! Scissor beats Paper");
-                        humanScore++;
-                    }
-                    else if(computerChoice == "rock"){
-                        console.log("You lose! Rock beats paper");
-                        computerScore++;
-                    }
-                    else{
-                        console.log("Tie");
-                    }
-                    break;
-        default:console.log("Invalid input");
+        }}
+        if (humanScore==5 || computerScore==5){
+            gameOver = true;
+            if(humanScore>computerScore)
+                {
+                    div.textContent = "Human won";
+                }
+                else{
+                    div.textContent = "Computer won";
+                }
+            console.log(humanScore);
+            console.log(computerScore);
+            btn1.removeEventListener("click",()=>playRound("rock", getComputerChoice()));
+            btn2.removeEventListener("click",()=>playRound("paper", getComputerChoice()));
+            btn3.removeEventListener("click",()=>playRound("scissor", getComputerChoice()));
 }
 }
+
 function playGame()
 {
-    let computerSelection = getComputerChoice();
-    let humanSelection = getHumanChoice()
-    playRound(humanSelection, computerSelection);
-    
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice()
-    playRound(humanSelection, computerSelection);
-
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice()
-    playRound(humanSelection, computerSelection);
-
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice()
-    playRound(humanSelection, computerSelection);
-
-    computerSelection = getComputerChoice();
-    humanSelection = getHumanChoice()
-    playRound(humanSelection, computerSelection);
-
-    if(humanScore>computerScore)
-    {
-        console.log("Human won");
+        const btn1 = document.querySelector("#btn1");
+        const btn2 = document.querySelector("#btn2");
+        const btn3 = document.querySelector("#btn3");
+        btn1.addEventListener("click",()=>playRound("rock", getComputerChoice()));
+        btn2.addEventListener("click",()=>playRound("paper",getComputerChoice()));
+        btn3.addEventListener("click",()=>playRound("scissor",getComputerChoice()));
+        buttons = document.querySelectorAll("button");
+        buttons.forEach((button)=>{
+            button.setAttribute("style","padding:10px;margin:10px;border-radius:5px");
+        div = document.querySelector("div");
+        div.setAttribute("style","font-weight:900;padding-left:10px");
+        }
+        )
     }
-    else{
-        console.log("Computer won");
-    }
-
-}
 playGame();
